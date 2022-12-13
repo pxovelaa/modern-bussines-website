4fac3d7002f44c0cc8c8b59f08925111cc5dac48
pxovelaa <gio.pxovelishvili@gmail.com>
2022-12-13 15:20:22 +0300

    Make Navbar , Hero and Billings sections

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
+
+const CardDeal = () => {
+  return (
+    <div>CardDeal</div>
+  )
+}
+
+export default CardDeal
